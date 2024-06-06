b8e1df8ac9d49246fd953e114fa98389fc54dbad
Julian Bierbaum <Julian Bierbaum>
2024-06-06 09:19:49 +0200

    added person class

--- schoolmanagement/src/lib-schoolmanagement/modules/person.cs
+++ schoolmanagement/src/lib-schoolmanagement/modules/person.cs
@@ -1,1 +1,22 @@
 namespace lib_schoolmanagement.person;
+
+internal abstract class Person {
+    protected string _name;
+    protected List<string> _classes;
+
+    internal string Name {
+        get { return _name; }
+        set { _name = value; }
+    }
+
+    internal List<string> Classes {
+        get { return _classes; }
+    }
+
+    internal Person(string name, List<string> classes) {
+        _name = name;
+        _classes = classes;
+    }
+
+    public abstract override string ToString();
+}
